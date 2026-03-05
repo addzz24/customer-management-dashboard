c1a2d80ea5d9190b64587a6a2169f48c187105c1
addzz24 <adityabhosale375@gmail.com>
2026-03-06 03:03:43 +0530

    cards added

--- src/app/features/key-performance-indicator/key-performance-indicator.ts
+++ src/app/features/key-performance-indicator/key-performance-indicator.ts
@@ -1,27 +1,62 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { Card } from '../../shared/components/card/card';
 import { DashboardStore } from '../../store/dashboard/dashboard.store';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-key-performance-indicator',
-  imports: [Card],
+  imports: [Card, CommonModule, MatIconModule],
   templateUrl: './key-performance-indicator.html',
   styleUrl: './key-performance-indicator.css',
-  standalone: true
+  standalone: true,
 })
 export class KeyPerformanceIndicator {
   store = inject(DashboardStore);
 
   performanceIndicators: any;
 
-  constructor(){
-    this.performanceIndicators = [
-      { name: 'Total Customers', value: this.store.totalCustomers() },
-      { name: 'Total Spend', value: `${this.store.totalSpend()} ₹` },
-      { name: 'Completed Orders', value: this.store.completedOrders() },
-      { name: 'Pending Orders', value: this.store.pendingOrders() }
-    ];
+  constructor() {
+    effect(() => {
+      this.updatePerformanceIndicators();
+    });
   }
 
+  updatePerformanceIndicators() {
+    const totalCustomers = this.store.totalCustomers();
+    const totalSpend = this.store.totalSpend();
+    const completed = this.store.completedOrders();
+    const pending = this.store.pendingOrders();
 
+    const totalOrders = completed + pending;
 
+    const orderTrend = totalOrders ? (completed / totalOrders) * 100 : 0;
+
+    this.performanceIndicators = [
+      {
+        name: 'Total Customers',
+        value: totalCustomers,
+        icon: 'people',
+        trend: ((completed - pending) / totalOrders) * 100,
+      },
+      {
+        name: 'Total Spend',
+        value: `${totalSpend} ₹`,
+        icon: 'monetization_on',
+        trend: (totalSpend / 10000) * 5,
+      },
+      {
+        name: 'Completed Orders',
+        value: completed,
+        icon: 'check_circle',
+        trend: orderTrend,
+      },
+      {
+        name: 'Pending Orders',
+        value: pending,
+        icon: 'pending',
+        trend: -(pending / totalOrders) * 100,
+      },
+    ];
+  }
 }
