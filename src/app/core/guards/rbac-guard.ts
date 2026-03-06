489c024eb69a7395c350504f6b9af6aecbf212c9
addzz24 <adityabhosale375@gmail.com>
2026-03-06 15:36:21 +0530

    route guard

--- src/app/core/guards/rbac-guard.ts
+++ src/app/core/guards/rbac-guard.ts
@@ -1,5 +1,19 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { RoleService } from '../services/role.service';
 
-export const rbacGuard: CanActivateFn = (route, state) => {
+export const rbacGuard: CanActivateFn = (route) => {
+
+  const roleService = inject(RoleService);
+  const router = inject(Router);
+
+  const allowedRoles = route.data?.['roles'] as string[];
+  const userRole = roleService.getRole();
+
+  if (allowedRoles && !allowedRoles.includes(userRole)) {
+    router.navigate(['/unauthorized']);
+    return false;
+  }
+
   return true;
 };
