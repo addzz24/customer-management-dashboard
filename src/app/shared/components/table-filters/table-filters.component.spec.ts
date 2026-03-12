0b14ee89c8212ca7810abb551770b542b3dd70cc
addzz24 <adityabhosale375@gmail.com>
2026-03-12 08:53:25 +0530

    table filter component unit tests added

--- src/app/shared/components/table-filters/table-filters.component.spec.ts
+++ src/app/shared/components/table-filters/table-filters.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TableFiltersComponent } from './table-filters.component';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('TableFiltersComponent', () => {
   let component: TableFiltersComponent;
@@ -8,9 +9,9 @@ describe('TableFiltersComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TableFiltersComponent]
-    })
-    .compileComponents();
+      imports: [TableFiltersComponent],
+      providers: [provideZonelessChangeDetection()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TableFiltersComponent);
     component = fixture.componentInstance;
@@ -19,5 +20,30 @@ describe('TableFiltersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize filters as empty object', () => {
+    expect(component.filters).toEqual({});
+  });
+
+  it('should emit filters when applyFilters is called', () => {
+    spyOn(component.apply, 'emit');
+
+    component.filters = { status: 'Completed' };
+    component.applyFilters();
+
+    expect(component.apply.emit).toHaveBeenCalledWith({
+      status: 'Completed',
+    } as any);
+  });
+
+  it('should reset filters and emit reset event', () => {
+    spyOn(component.reset, 'emit');
+
+    component.filters = { status: 'Completed' };
+    component.resetFilters();
+
+    expect(component.filters).toEqual({});
+    expect(component.reset.emit).toHaveBeenCalled();
   });
 });
