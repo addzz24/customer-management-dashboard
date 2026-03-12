5bd503e1639a830e32fb2fb67e42aa2bff4b3f13
addzz24 <adityabhosale375@gmail.com>
2026-03-12 08:05:00 +0530

    test cases added for layout and feature components.

--- src/app/core/services/role.service.spec.ts
+++ src/app/core/services/role.service.spec.ts
@@ -1,16 +1,43 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RoleService } from './role.service';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('Role', () => {
   let service: RoleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideZonelessChangeDetection()],
+    });
     service = TestBed.inject(RoleService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return default role as admin', () => {
+    const role = service.getRole();
+    expect(role).toBe('admin');
+  });
+
+  it('should update role to manager', () => {
+    service.setRole('manager');
+
+    expect(service.getRole()).toBe('manager');
+  });
+
+  it('should update role to viewer', () => {
+    service.setRole('viewer');
+
+    expect(service.getRole()).toBe('viewer');
+  });
+
+  it('should update previous role when setRole is called again', () => {
+    service.setRole('manager');
+    service.setRole('viewer');
+
+    expect(service.getRole()).toBe('viewer');
   });
 });
--- src/app/features/analytics/analytics.component.spec.ts
+++ src/app/features/analytics/analytics.component.spec.ts
@@ -1,16 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AnalyticsComponent } from './analytics.component';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { GlobalStore } from '../../store/global/global.store';
 
 describe('AnalyticsComponent', () => {
   let component: AnalyticsComponent;
   let fixture: ComponentFixture<AnalyticsComponent>;
+  let mockStore: any;
+  let mockSpendByCategoryValue;
+  let mockSpendByStatusValue;
+  let mockSpendByDateValue;
 
   beforeEach(async () => {
+    mockSpendByCategoryValue = { category: 'Food', amount: 200 };
+    mockSpendByStatusValue = { status: 'Completed', amount: 500 };
+    mockSpendByDateValue = { date: '2026-03-01', amount: 100 };
+    mockStore = {
+      spendByCategory: jasmine.createSpy().and.returnValue([mockSpendByCategoryValue]),
+      spendByStatus: jasmine.createSpy().and.returnValue([mockSpendByStatusValue]),
+      spendTrendByDate: jasmine.createSpy().and.returnValue([mockSpendByDateValue]),
+    };
+
     await TestBed.configureTestingModule({
-      imports: [AnalyticsComponent]
-    })
-    .compileComponents();
+      imports: [AnalyticsComponent],
+      providers: [provideZonelessChangeDetection(), { provide: GlobalStore, useValue: mockStore }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AnalyticsComponent);
     component = fixture.componentInstance;
@@ -20,4 +35,47 @@ describe('AnalyticsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get spend by category from store', () => {
+    const result = component.spendByCategory();
+    expect(result.length).toBe(1);
+    expect(mockStore.spendByCategory).toHaveBeenCalled();
+  });
+
+  it('should get spend by status from store', () => {
+    const result = component.spendByStatus();
+    expect(result.length).toBe(1);
+    expect(mockStore.spendByStatus).toHaveBeenCalled();
+  });
+
+  it('should get spend trend data from store', () => {
+    const result = component.spendTrendByDate();
+    expect(result.length).toBe(1);
+    expect(mockStore.spendTrendByDate).toHaveBeenCalled();
+  });
+
+  it('should emit status filter', () => {
+    spyOn(component.currentFilter, 'emit');
+    component.onStatusFilter('Completed');
+    expect(component.currentFilter.emit).toHaveBeenCalledWith({
+      status: 'Completed',
+    });
+  });
+
+  it('should emit category filter', () => {
+    spyOn(component.currentFilter, 'emit');
+    component.onCategoryFilter('Food');
+    expect(component.currentFilter.emit).toHaveBeenCalledWith({
+      category: 'Food',
+    });
+  });
+
+  it('should emit date filter', () => {
+    spyOn(component.currentFilter, 'emit');
+    component.onDateFilter('2026-03-01');
+    expect(component.currentFilter.emit).toHaveBeenCalledWith({
+      startDate: '2026-03-01',
+    });
+  });
+
 });
